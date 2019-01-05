53736e1cfeaf76c97124105c4c75fb135ef30796
Stanislav Evglevskiy <evglevsckij.stanislav@gmail.com>
2019-01-05 14:01:11 +0300

    mail error fix

--- bin/entrypoints/php/custom-start.sh
+++ bin/entrypoints/php/custom-start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#copy config
-cp /etc/.msmtp_php_original /etc/.msmtp_php
-chmod 600 /etc/.msmtp_php 
-chown www-data:www-data /etc/.msmtp_php
-
 #create  msmtp logdir
 mkdir -p /var/log/msmtp/ && chmod 777 /var/log/msmtp/
 
@@ -14,5 +9,10 @@ mkdir -p /var/log/php/ && chmod 777 /var/log/php/
 #change container user permissions
 usermod -u $user_uid www-data
 groupmod -g $user_gid www-data
+
+#copy config
+cp /etc/.msmtp_php_original /etc/.msmtp_php
+chmod 600 /etc/.msmtp_php 
+chown www-data:www-data /etc/.msmtp_php
 
 exec docker-php-entrypoint "$@"
