ca6546b59f2b90e8052d7d469e24515b7de285de
Станислав <evglevsckij.stanislav@gmail.com>
2019-01-09 15:27:06 +0300

    removed hardcode

--- export_variables.sh
+++ export_variables.sh
@@ -9,6 +9,8 @@ touch ~/$variablesList
 
 echo "export REMOTE_HOST_IP=\"$(echo $(hostname -I) | cut -d ' ' -f 1)\"" >> ~/$variablesList
 echo "export DEV_PATH=\"$(pwd)\"" >> ~/$variablesList
+echo "export user_uid=\"$(id -u)\"" >> ~/$variablesList
+echo "export user_gid=\"$(id -g)\"" >> ~/$variablesList
 
 if ! grep -q "source ~/$variablesList" ~/.bashrc; then
   echo "source ~/$variablesList" >> ~/.bashrc
