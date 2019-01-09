294f9cfac39a071d83c5e29b8202414f7e7e2499
Станислав <evglevsckij.stanislav@gmail.com>
2019-01-09 14:35:22 +0300

    crosspaltform fixes

--- bin/entrypoints/php/custom-start.sh
+++ bin/entrypoints/php/custom-start.sh
@@ -6,10 +6,6 @@ mkdir -p /var/log/msmtp/ && chmod 777 /var/log/msmtp/
 #php log dir
 mkdir -p /var/log/php/ && chmod 777 /var/log/php/
 
-#change container user permissions
-usermod -u $user_uid www-data
-groupmod -g $user_gid www-data
-
 #copy config
 cp /etc/.msmtp_php_original /etc/.msmtp_php
 chmod 600 /etc/.msmtp_php 
