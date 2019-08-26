fb1c769ccd2b677ff9f1a5a9f03e960b105db6c3
Станислав <evglevsckij.stanislav@gmail.com>
2019-08-26 16:03:24 +0300

    removed node.js installing added soap into php7-fpm

--- setup.sh
+++ setup.sh
@@ -33,15 +33,4 @@ for filename in ./local_tools/*; do
     sudo chmod +x /usr/local/bin/$(basename "$filename")
 done
 
-
-
-#installing node js and npm
-nodejs=$(dpkg-query -W --showformat='${Status}\n' nodejs|grep "install ok installed")
-npm=$(dpkg-query -W --showformat='${Status}\n' npm|grep "install ok installed")
-if [ "" == "$npm" -o "" == "$nodejs" ]; then
-  echo "We need to install npm or nodejs"
-  sudo apt-get update -y
-  sudo apt-get install -y nodejs
-  sudo apt-get install -y npm
-fi
 echo "Done"
